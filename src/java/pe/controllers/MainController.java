60577e865e0d88c2eb7bb10ef3ca14b1e2880818
okeco727 <yuukirito1602@gmail.com>
2026-06-24 08:11:15 +0700

    update

--- src/java/pe/controllers/MainController.java
+++ src/java/pe/controllers/MainController.java
@@ -6,6 +6,7 @@
 package pe.controllers;
 
 import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,19 +16,11 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Computing Fundamental - HCM Campus
  */
+@WebServlet(name = "MainController", urlPatterns = {"/MainController"})
 public class MainController extends HttpServlet {
 
     private static final String WELCOME = "login.jsp";
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
